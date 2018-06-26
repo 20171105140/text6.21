e28de6f9ed726c30ff129618a02ab28d4e2d4bfd
20171105140 <s20171105140@Mac02.local>
2018-06-26 14:16:28 +0800

    hello word
    hello word

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  text6.21
+//
+//  Created by 张学飞 on 18/6/21.
+//  Copyright © 2018年 张学飞. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
